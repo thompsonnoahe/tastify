c34c27a9d9b21d645fc1c8930b90a918d070e10f
Noah Thompson <thompsonnoahe@gmail.com>
2021-08-31 15:11:33 -0700

    Env vars

--- src/apis/auth0.js
+++ src/apis/auth0.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import {} from 'dotenv/config';
 
 console.log(process.env);
 
 export default axios.create({
   baseURL: 'https://tastify.us.auth0.com/api/v2',
   headers: {
-    Authorization: `${process.env.token_type} ${process.env.access_token}`,
+    Authorization: `${process.env.REACT_APP_AUTH0_TOKEN_TYPE} ${process.env.REACT_APP_AUTH0_ACCESS_TOKEN}`,
   },
 });
