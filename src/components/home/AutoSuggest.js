e2b5235b0e42e34a2504b82e6808f46b45c0e2a1
Noah Thompson <thompsonnoahe@gmail.com>
2021-09-01 10:32:26 -0700

    Imporvements to autosuggest

--- src/components/home/AutoSuggest.js
+++ src/components/home/AutoSuggest.js
@@ -1,17 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { autoComplete } from '../../actions';
 import { connect } from 'react-redux';
 
 const AutoSuggest = props => {
   const [suggestions, setSuggestions] = useState([]);
   const [hide, setHide] = useState(false);
+  const suggestionsBox = useRef();
+
+  const hideOnOutsideClick = () => {
+    document.addEventListener('click', e => {
+      if (e.target !== suggestionsBox.current) {
+        setHide(true);
+      }
+    });
+  };
+
+  useEffect(() => {
+    hideOnOutsideClick();
+  });
+
   useEffect(() => {
     props.autoComplete(props.value);
     setSuggestions(props.suggestions);
     setHide(false);
   }, [props.value]);
+
   return suggestions.length !== 0 ? (
     <div
+      ref={suggestionsBox}
       className='tile is-parent ml-10 p-1'
       style={{ display: `${hide ? 'none' : ''}` }}>
       <div className='tile is-child box'>
