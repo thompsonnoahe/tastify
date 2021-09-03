ed91b5a40851c55dd2037c51bf39f0f205428c81
Noah Thompson <thompsonnoahe@gmail.com>
2021-09-03 16:45:29 -0700

    Debugging

--- src/apis/auth0.js
+++ src/apis/auth0.js
@@ -5,7 +5,6 @@ console.log(process.env);
 export default axios.create({
   baseURL: 'https://tastify.us.auth0.com/api/v2',
   headers: {
-    Authorization: `${process.env.REACT_APP_AUTH0_TOKEN_TYPE} ${process.env.REACT_APP_AUTH0_ACCESS_TOKEN}
-    `,
+    Authorization: `${process.env.REACT_APP_AUTH0_TOKEN_TYPE} ${process.env.REACT_APP_AUTH0_ACCESS_TOKEN}`,
   },
 });
